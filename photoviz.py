086af5992851d76e1115f471dbac440562c4c259
Yijun Ma <mayijun1203@gmail.com>
2024-01-23 14:42:38 +0800

    update

--- photoviz.py
+++ photoviz.py
@@ -23,13 +23,13 @@ pd.options.display.max_columns=100
 
 
 
-# Convert HEIC to JPG
-for i in os.listdir(path+'original/Yangfei'):
-    pillow_heif.register_heif_opener()
-    imgpath=path+'original/Yangfei/'+i
-    img=PIL.Image.open(imgpath)
-    exif_dict=pillow_heif.read_heif(imgpath).info['exif']
-    img.save(path+'original/Yangfei/'+i.replace('HEIC','JPG'),exif=exif_dict)
+# # Convert HEIC to JPG
+# for i in os.listdir(path+'original/Yangfei'):
+#     pillow_heif.register_heif_opener()
+#     imgpath=path+'original/Yangfei/'+i
+#     img=PIL.Image.open(imgpath)
+#     exif_dict=pillow_heif.read_heif(imgpath).info['exif']
+#     img.save(path+'original/Yangfei/'+i.replace('HEIC','JPG'),exif=exif_dict)
 
 
 
@@ -45,7 +45,7 @@ def decimalcoords(orgcoords,ref):
 
 # Execution
 df=[]
-for i in ['Yijun','Yangfei']:
+for i in ['Yijun','Yangfei','Evonne']:
     for j in os.listdir(path+'original/'+i):
         with open(path+'original/'+i+'/'+j,'rb') as src:
             img=exif.Image(src)
@@ -57,9 +57,9 @@ for i in ['Yijun','Yangfei']:
                         decimalcoords(img.gps_longitude,
                                       img.gps_longitude_ref))
             except AttributeError:
-                print(imgpath+' No Coordinates!')
+                print(i+'/'+j+' No Coordinates!')
         else:
-            print(imgpath+' No EXIF!')
+            print(i+'/'+j+' No EXIF!')
         tp=pd.DataFrame({'author':[i],
                          'photo':[j],
                          'datetime':[img.datetime_original],
@@ -76,7 +76,7 @@ df.to_file(path+'photoattr.geojson',crs=4326, driver='GeoJSON')
 
 
 # Compress and rotate photos
-for i in ['Yijun','Yangfei']:
+for i in ['Yijun','Yangfei','Evonne']:
     for j in os.listdir(path+'original/'+i):
         tp=PIL.Image.open(path+'original/'+i+'/'+j)
         ort=df.loc[((df['author']==i)&(df['photo']==j)),'orientation'][0]
